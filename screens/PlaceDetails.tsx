32b468f3daaedd319270a1c7f8bd000a85d7ef98
Matilda Mared <matildamared@live.se>
2023-01-09 10:11:43 +0100

    fetch place details data on component load

--- screens/PlaceDetails.tsx
+++ screens/PlaceDetails.tsx
@@ -1,12 +1,15 @@
 import { RouteProp, useRoute } from "@react-navigation/native";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ScrollView, Image, View, StyleSheet, Text } from "react-native";
 import { StackParamList } from "../App";
 import OutlinedButton from "../components/UI/OutlinedButton";
 import { Colors } from "../constants/colors";
+import { Place } from "../types/Place";
+import { fetchPlaceDetails } from "../utils/database";
 
 export default function PlaceDetails() {
 	const route = useRoute<RouteProp<StackParamList, "PlaceDetails">>();
+	const [place, setPlace] = useState<Place>();
 	const selectedPlaceId = route.params?.placeId;
 
 	function showOnMapHandler() {}
@@ -14,22 +17,26 @@ export default function PlaceDetails() {
 	useEffect(() => {
 		if (!selectedPlaceId) {
 			return;
-        }
-        
-        async function fetchPlace() {
+		}
 
-        }
+		async function fetchPlace() {
+			const data = await fetchPlaceDetails(selectedPlaceId);
+			setPlace(data);
+		}
 
-        fetchPlace();
-		console.log("selectedPlaceId", selectedPlaceId);
+		fetchPlace();
 	}, [selectedPlaceId]);
+
+	if (!place) {
+		return null;
+	}
 
 	return (
 		<ScrollView>
 			<Image style={styles.image} />
 			<View style={styles.locationContainer}>
 				<View style={styles.addressContainer}>
-					<Text style={styles.address}>Address</Text>
+					<Text style={styles.address}>{place.address}</Text>
 				</View>
 				<OutlinedButton icon="map" onPress={showOnMapHandler}>
 					View on Map
